db49a1f206846bb121a277c1ec895bed62a9c3db
Jakub Sobczak <jsobczak1@gmail.com>
2020-02-25 12:07:39 +0100

    First commit

---
+++ Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // This application is supposed to help us to select type of burgers,
+        // some of the additional items (additions) to be added to the burger
+        // and calculate its pricing.
+
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthAddition1("Lentils", 3.41);
+        System.out.println("Total Healthy Burger price is  " + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.addHamburgerAddition3("Should not do this", 50.53);
+        db.itemizeHamburger();
+
+
+    }
+
+
+}
+
